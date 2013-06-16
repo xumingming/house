13ca037db7ab9db8ade86ceefb37a11caa0c78d7
James Xu <xumingming64398966@gmail.com>
2013-06-16 21:45:32 +0800

    extracted building data into a seperate def

--- src/house/handler.clj
+++ src/house/handler.clj
@@ -22,14 +22,39 @@
     :lvhualv 0.3395
     :rongjilv 2.41
     :gongtanmianji 10
+    :zhuangxiu "毛坯"
+    :hushu 900
+    :cheweishu 871
     }
    
    "xinhu-guolin"
    {:name "新湖果岭"
     :loc ""
     :touzishan "浙江新兰得置业有限公司"
-    :kaifashan ""
-    :wuye "浙江新湖物业管理有限公司"}}
+    :kaifashan "浙江新兰得置业有限公司"
+    :wuye "浙江新湖物业管理有限公司"
+    :wuyefei 2
+    :price 10000
+    :edu ["杭州英特外国语学校"
+          "余杭镇第一中学教育集团镇中校区"
+          "余杭镇第一幼儿园"
+          "龙文教育"
+          "余杭中学"
+          "国泰·西园幼儿园"
+          "杭州波达塑业有限公司研究开发中心"
+          "阳光宝贝方汇幼儿园"
+          "杭州绿城育华桃花源学校"
+          "国泰·西园儿童之家"]
+    :jiaotong "5号线工教路站"
+    :kaipan-date "2011-10"
+    :jiaofu-date "2014-06"
+    :lvhualv 0.36
+    :rongjilv 1.2
+    :gongtanmianji 10
+    :zhuangxiu "毛坯"
+    :hushu 2330
+    :cheweishu 2302
+    }}
   )
 (defroutes app-routes
   (GET "/loan.htm" {params :params} []
@@ -60,8 +85,12 @@
                  :monthes (range loan-month))))
   (GET "/" [] (str "test"))
   (GET "/building/:name" [name]
-       (let [building (buildings name)]
+       (let [building (buildings name)
+             id->name (into {} (map (fn [pair] [(key pair) (:name (val pair))]) buildings))]
+         (println id->name)
          (render "building.vm"
+                 :id name
+                 :id-to-name id->name
                  :name (:name building)
                  :loc (:loc building)
                  :touzishan (:touzishan building)
