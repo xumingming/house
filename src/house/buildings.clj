c34e6c2acaa65e8a6ad9724bf8222f713ef252b9
James Xu <xumingming64398966@gmail.com>
2013-06-16 21:47:32 +0800

    extract buildings data into seperate file

---
+++ src/house/buildings.clj
@@ -0,0 +1,52 @@
+(ns house.buildings)
+
+(def buildings
+  {"xixi-haixiaohai"
+   {:name "西溪海小海"
+    :loc "天目山西路北侧，闲富北路与上和路交叉口西南角"
+    :touzishan ""
+    :kaifashan ""
+    :wuye "杭州金成物业公司"
+    :wuyefei 1.8
+    :price 9800
+    :edu "小学"
+    :jiaotong ""
+    :kaipan-date "2013-04"
+    :jiaofu-date "2014-12"
+    :lvhualv 0.3395
+    :rongjilv 2.41
+    :gongtanmianji 10
+    :zhuangxiu "毛坯"
+    :hushu 900
+    :cheweishu 871
+    }
+   
+   "xinhu-guolin"
+   {:name "新湖果岭"
+    :loc ""
+    :touzishan "浙江新兰得置业有限公司"
+    :kaifashan "浙江新兰得置业有限公司"
+    :wuye "浙江新湖物业管理有限公司"
+    :wuyefei 2
+    :price 10000
+    :edu ["杭州英特外国语学校"
+          "余杭镇第一中学教育集团镇中校区"
+          "余杭镇第一幼儿园"
+          "龙文教育"
+          "余杭中学"
+          "国泰·西园幼儿园"
+          "杭州波达塑业有限公司研究开发中心"
+          "阳光宝贝方汇幼儿园"
+          "杭州绿城育华桃花源学校"
+          "国泰·西园儿童之家"]
+    :jiaotong "5号线工教路站"
+    :kaipan-date "2011-10"
+    :jiaofu-date "2014-06"
+    :lvhualv 0.36
+    :rongjilv 1.2
+    :gongtanmianji 10
+    :zhuangxiu "毛坯"
+    :hushu 2330
+    :cheweishu 2302
+    }}
+  )
